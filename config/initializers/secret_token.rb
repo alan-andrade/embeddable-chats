4eeba979cebd2103778f7262e359ffe6ff61f066
Alan Andrade <alan.andradec@gmail.com>
2013-09-01 21:58:48 -0700

    rails 4 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmbeddableChats::Application.config.secret_key_base = '53c7718b252164dd9452a52e533ae3479f77eca5ff09985fdb431dd729570be909cfbe25e695aa86997f3703ec17aa92529cf7f8daaf2209b7d02ae0b6f062ef'
